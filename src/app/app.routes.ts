db975fa726aae97ab4a1e984f5650a071feabe4a
22520187 <22520187@gm.uit.edu.vn>
2025-10-24 11:28:05 +0700

    feat: setup structure for project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from "./auth/login/login.component";
-import { SignupComponent } from "./auth/signup/signup.component";
+import { LoginComponent } from "./pages/auth/login/login.component";
+import { SignupComponent } from "./pages/auth/signup/signup.component";
 export const routes: Routes = [
     { path: '', redirectTo: '/login', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
